c984b70ff4d4377f8ebead6700e6061024fd0c0c
jnopareboateng <jnopareboateng@outlook.com>
2024-03-11 12:31:58 +0000

    Remove Footer component import and delete package.json

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./(components)/Header";
-import Footer from "./(components)/Footer";
+// import Footer from "./(components)/Footer";
 import { InriaRegular } from "./(fonts)/font";
 
 const inter = Inter({ subsets: ["latin"] });
