b13683f4bff14f1c2aed893efc74aa483db2b9ee
jnopareboateng <jnopareboateng@outlook.com>
2024-03-12 12:16:46 +0000

    added homepage route

---
+++ app/homepage/page.jsx
@@ -0,0 +1,20 @@
+// Import your section components
+import HeroSection from "../(components)/HeroSection.jsx";
+import FeaturesSection from "../(components)/FeaturesSection";
+import TherapySection from "../(components)/TherapySection";
+import LastSection from "../(components)/LastSection";
+
+export default function HomePage() {
+  return (
+    <>
+    {/* <Header /> */}
+    {/* <HeroSection /> */}
+      <FeaturesSection />
+      {/* <TherapySection /> */}
+      {/* <LastSection /> */}
+      {/* <Footer /> */}
+      {/* <OtherSection /> */}
+      {/* Add other sections here */}
+    </>
+  );
+}
