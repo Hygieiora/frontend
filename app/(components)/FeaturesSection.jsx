2847ee250ffd7023dc36347f0ec335ef4a4af300
TravisBoltz <fariduoubdamusah@gmail.com>
2024-03-12 16:52:57 +0000

    FeaturesSection1

--- app/(components)/FeaturesSection.jsx
+++ app/(components)/FeaturesSection.jsx
@@ -1,25 +1,25 @@
 // Import necessary dependencies
 import { FaLock } from "react-icons/fa"; // Import the icons you need
-
+import Image from "next/image";
 // Sample data for the features/services, replace with your actual data
 const features = [
   {
     icon: <FaLock className="text-blue-500" />, // Replace with your actual icons
     title: "Crisis intervention",
     description:
-      "Discover our crisis research such as hotlines, emergency contacts, and tips for recognizing crisis and reaching out safely.",
+     "Experience the future of therapy with Hygieiora - a unique space for individuals to connect with professional therapists and volunteers in fostering a supportive community dedicated to enhancing mental well-being "
   },
   // ... Add other features/services here
 ];
 
 export default function FeaturesSection() {
   return (
-    <div className="p-8 bg-blue-900 text-white">
-      <h2 className="text-2xl mb-6">
-        Welcome to a space of support and healing
+    <section className="p-8 bg-blue-600 text-white">
+      <h2 className="text-4xl ">
+        <span className="text-blue-900">It's okay</span> not <br/>to be okay.
       </h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid items-center justify-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {features.map((feature, index) => (
           <div key={index}>
             <div className="flex items-center mb-2">
@@ -27,12 +27,13 @@ export default function FeaturesSection() {
               <h3 className="text-xl ml-2">{feature.title}</h3>
             </div>
             <p className="mb-4">{feature.description}</p>
-            <button className="border border-white text-white rounded px-4 py-2">
+            {/* <button className="border border-white text-white rounded px-4 py-2">
               View more
-            </button>
+            </button> */}
           </div>
         ))}
+        <Image src={'/phones.png'} className="-mt-14" width={200} height={200}></Image>
       </div>
-    </div>
+    </section>
   );
 }
