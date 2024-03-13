a6543072427ae8e376f405f95cc5a4f2bbe9a46a
TravisBoltz <fariduoubdamusah@gmail.com>
2024-03-13 02:27:04 -0700

    LastSection

--- app/(components)/FeaturesSection.jsx
+++ app/(components)/FeaturesSection.jsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 // Sample data for the features/services, replace with your actual data
 const features = [
   {
-    icon: <FaLock className="text-blue-500" />, // Replace with your actual icons
-    title: "Crisis intervention",
+    icon: <FaLock className="text-white" />, // Replace with your actual icons
+    title: "Hygieiora Inc",
     description:
-     "Experience the future of therapy with Hygieiora - a unique space for individuals to connect with professional therapists and volunteers in fostering a supportive community dedicated to enhancing mental well-being "
+      "Experience the future of therapy with Hygieiora - a unique space for individuals to connect with professional therapists and volunteers in fostering a supportive community dedicated to enhancing mental well-being ",
   },
   // ... Add other features/services here
 ];
@@ -16,10 +16,11 @@ export default function FeaturesSection() {
   return (
     <section className="p-8 bg-blue-600 text-white">
       <h2 className="text-4xl ">
-        <span className="text-blue-900">It's okay</span> not <br/>to be okay.
+        <span className="text-blue-900">It's okay</span> not <br />
+        to be okay.
       </h2>
 
-      <div className="grid items-center justify-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="flex items-center mx-auto justify-center">
         {features.map((feature, index) => (
           <div key={index}>
             <div className="flex items-center mb-2">
@@ -32,7 +33,12 @@ export default function FeaturesSection() {
             </button> */}
           </div>
         ))}
-        <Image src={'/phones.png'} className="-mt-14" width={200} height={200}></Image>
+        <Image
+          src={"/phones.png"}
+          className="-mt-14"
+          width={200}
+          height={200}
+        ></Image>
       </div>
     </section>
   );
--- app/(components)/LastSection.jsx
+++ app/(components)/LastSection.jsx
@@ -1,17 +1,40 @@
 // Import necessary dependencies
+import Image from "next/image";
 import { FaArrowRight } from "react-icons/fa"; // Import the icons you need
 
 export default function LastSection() {
   return (
-    <div className="p-8 bg-white text-black">
-      <h2 className="text-2xl mb-6">Ready to get started?</h2>
-      <p className="mb-8">
-        Join us today and start your journey to a better life.
-      </p>
-
-      <button className="bg-blue-500 text-white rounded px-4 py-2 flex items-center">
-        Start Now <FaArrowRight className="ml-2" />
-      </button>
-    </div>
+    <>
+      <section className="p-8 bg-white flex items-center justify-items-center flex-col text-black">
+        <h1 className="text-2xl font-bold mb-2">Here's how to get therapy</h1>
+        <p className="text-sm mb-8">Your health your call </p>
+        <section className="flex justify-between gap-5 mx-auto flex-row">
+          <div className="flex flex-col  items-center text-center">
+            <Image href={"/no1.png"} width={60} height={60} alt={"no1"}></Image>
+            <h2 className="font-bold">Student Id or email</h2>
+            <p className="text-blue-600 text-sm">
+              Sign up or login with your details to continue therapy{" "}
+            </p>
+          </div>
+          <div className="flex flex-col items-center text-center">
+            <Image href={"/no2.png"} width={60} height={60} alt={"no2"}></Image>
+            <h2 className="font-bold">Talk to therapists</h2>
+            <p className="text-blue-600 text-sm">
+              Tell as your issues, we will find you a therapist to talk to
+            </p>
+          </div>
+          <div className="flex flex-col  items-center text-center">
+            <Image href={"/no3.png"} width={60} height={60} alt={"no3"}></Image>
+            <h2 className="font-bold">Stay connected with Hygieiora</h2>
+            <p className="text-blue-600 text-sm">
+              Stay connected to therapist and volunteers{" "}
+            </p>
+          </div>
+        </section>
+        <button className="p-3 font-semi-bold text-blue-600 border border-blue-600 rounded-full px-10 font-semibold mt-10">
+          Start Now
+        </button>
+      </section>
+    </>
   );
 }
