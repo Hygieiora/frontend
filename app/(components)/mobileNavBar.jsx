28ea21882dcb4dbaf284b0a848a16ec87fd093d6
TravisBoltz <fariduoubdamusah@gmail.com>
2024-03-24 07:18:35 -0700

    carousel button correction and border on mobilenav

--- app/(components)/mobileNavBar.jsx
+++ app/(components)/mobileNavBar.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const MobileNavBar = () => {
   return (
-    <div className="  fixed bottom-0 left-0 w-full bg-blue-600 flex justify-around py-4 px-6 z-50 rounded-t-3xl md:hidden">
+    <div className="border-2 border-white fixed bottom-0 left-0 w-full bg-blue-600 flex justify-around py-4 px-6 z-50 rounded-t-3xl md:hidden">
       <Link href="/">
         <Image width={30} height={30} src="/assets/DashBoard.png" alt="Home" />
       </Link>
