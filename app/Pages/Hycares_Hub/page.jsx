a354418c3e41ce374ee51df00385282d48f3aeaf
jnopareboateng <jnopareboateng@outlook.com>
2024-03-26 12:15:33 +0000

    ENHANCED COMMENTS

--- app/Pages/Hycares_Hub/page.jsx
+++ app/Pages/Hycares_Hub/page.jsx
@@ -1,12 +1,18 @@
 "use client";
 import React, { useState } from "react";
-import { Tabs, Tab } from "@nextui-org/tabs";
+// import { Tabs, Tab } from "@nextui-org/tabs";
 import SideNav from "../../(components)/sideNav";
 import UpcomingEvents from "../../(components)/Hycares_Hub/UpcomingEvents";
 import FeaturedArticles from "../../(components)/Hycares_Hub/FeaturedArticles";
 import MobileNavBar from "../../(components)/mobileNavBar";
 import Header from "../../(components)/Header";
 
+/**
+ * Represents a page in the Hycares Hub section of a website.
+ * Displays a header, a side navigation bar, a mobile navigation bar, and a main content area.
+ * The main content area includes a title and is currently empty.
+//  * @returns {JSX.Element} The JSX element representing the page layout of the Hycares Hub section.
+ */
 const HycaresHub = () => {
   const [activeTab, setActiveTab] = useState("Upcoming Events");
 
@@ -24,13 +30,22 @@ const HycaresHub = () => {
 
   return (
     <>
+      {/* Header component */}
       <Header isFirstPage={false} />
+
+      {/* Main content */}
       <main className="flex h-screen overflow-hidden">
+        {/* Side navigation bar */}
         <SideNav />
+
+        {/* Mobile navigation bar */}
         <MobileNavBar />
+
+        {/* Main content area */}
         <div className="p-4 flex flex-col justify-center w-full">
-          <h1 className="text-center text-2xl font-bold mb-4">HyCare HUB</h1></div>
-        
+          <h1 className="text-center text-2xl font-bold mb-4">HyCare HUB</h1>
+          {/* Content goes here */}
+        </div>
       </main>
     </>
   );
